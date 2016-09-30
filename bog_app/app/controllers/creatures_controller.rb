b52049bdec0ae9c06270d30764aee12c84598d26
ashleyvernon <avernon526@gmail.com>
2016-09-30 15:01:11 -0700

    added routes for new create show and controllers for each

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -7,4 +7,27 @@ class CreaturesController < ApplicationController
 		#remembers default behavior is to render :index
 	end
 
+	#show the new creature form
+	def new
+		@creature = Creature.new
+		#remember the default behavior is to render :new
+	end
+
+	#create a new creature in the database
+	def create
+	#whitelist params and save them to a variable
+		creature_params = params.require(:creature).permit(:name, :description)
+
+	#create a new creature from 'creature_params'
+		creature = Creature.new(creature_params)
+
+	#if creature saves, redirect to route that displays all creatures
+		if creature.save
+			redirect_to creatures_path
+		#redirect_to creatures_path is equivalent to:
+		#redirect_to "/creatures"
+		end
+	end
+
+
 end
