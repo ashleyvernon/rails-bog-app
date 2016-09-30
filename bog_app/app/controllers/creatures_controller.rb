9ac0ffe30c5290d8ef7576be1f10f5cf023168ef
ashleyvernon <avernon526@gmail.com>
2016-09-30 16:03:48 -0700

    routes for edit and update along with controllers

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -15,18 +15,62 @@ class CreaturesController < ApplicationController
 
 	#create a new creature in the database
 	def create
-	#whitelist params and save them to a variable
-		creature_params = params.require(:creature).permit(:name, :description)
 
 	#create a new creature from 'creature_params'
 		creature = Creature.new(creature_params)
 
-	#if creature saves, redirect to route that displays all creatures
+	#if creature saves, redirect to route that displays ONLY 
+	#the newly created creature
 		if creature.save
-			redirect_to creatures_path
+			redirect_to creature_path(creature)
 		#redirect_to creatures_path is equivalent to:
 		#redirect_to "/creatures"
 		end
+	end
+
+	#display a specific creature
+	def show
+		#get the creature id from the url params
+		creature_id = params[:id]
+
+		#use the 'creature_id' to find the creature in the database
+		#and save it to an instance variable
+		@creature = Creature.find_by_id(creature_id)
+
+		#render the show view (it has access to instance variable)
+		#remember the default behavior is to render :show
+	end
+
+	def edit
+		#get the creature id from the url params
+		creature_id = params[:id]
+
+		#use 'creature_id' to find the creature in the databse
+		#and save it to an instance variable
+		@creature = Creature.find_by_id(creature_id)
+	end
+
+	def update
+		#get the creature id from the url params
+		creature_id = params[:id]
+
+		#use creature id to find hte creature in the database
+		#and save it to an instance variable
+		creature = Creature.find_by_id(creature_id)
+
+		#update the creature
+		creature.update_attributes(creature_params)
+
+		#redirect to show page for the updated creature
+		redirect_to creature_path(creature)
+		#this is equivalent to: redirect_to "/creatures/#{creature.id}"
+	end
+
+	private
+
+	def creature_params
+		#whitelist params return whitelisted version
+		params.require(:creature).permit(:name, :description)
 	end
 
 
