e7c9821527e5a0126a494a077be710afa40062ed
ashleyvernon <avernon526@gmail.com>
2016-10-02 10:06:57 -0700

    styling to edit creature, link to all creatures

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root to: "creatures#index"
+	root "creatures#index"
 
 	get "/creatures", to: "creatures#index", as: "creatures"
 	get "/creatures/new", to: "creatures#new", as: "new_creature"
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 	get "/creatures/:id", to: "creatures#show", as: "creature"
 	get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
 	patch "/creatures/:id", to: "creatures#update", as: "update_creature"
+	delete "/creatures/:id", to: "creatures#destroy", as: "destroy_creature"
 end
